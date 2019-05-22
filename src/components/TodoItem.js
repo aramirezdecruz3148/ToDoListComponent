6a386869426b3392eff016f61b298bd18ee71e7b
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-22 09:09:09 -0700

    successfully DDD-ed my TodoItem component

---
+++ src/components/TodoItem.js
@@ -0,0 +1,15 @@
+import Component from './Component.js';
+
+class TodoItem extends Component {
+    renderTemplate() {
+        const todo = this.props.todo;
+        return /*html*/`
+        <label for="${todo.label}">
+            <input checked value="${todo.label}" type="checkbox"> ${todo.task}
+        </label>
+        `;
+    }
+
+}
+
+export default TodoItem;

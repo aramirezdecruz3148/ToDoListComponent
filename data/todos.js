a6f307aeee08eec105f346e5d2f59c960e60e710
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-21 19:31:53 -0700

    set up my configs, also added most files I will need for the project in the appropriate folders

---
+++ data/todos.js
@@ -0,0 +1,29 @@
+const todos = [
+    {
+        task: 'Wash dog',
+        label: 'wash-dog',
+        completed: true
+    },
+    {
+        task: 'Clean house',
+        label: 'clean-house',
+        completed: false
+    },
+    {
+        task: 'Do laundry',
+        label: 'do-laundry',
+        completed: false
+    },
+    {
+        task: 'Build personal website',
+        label: 'build-personal-website',
+        completed: false
+    },
+    {
+        task: 'Embrioder mask',
+        label: 'embroider-mask',
+        completed: false
+    }
+];
+
+export default todos;

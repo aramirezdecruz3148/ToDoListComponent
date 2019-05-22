c038d51e4fa77a625ab9448598214209c8312e82
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-22 11:02:40 -0700

    finished styling my components

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,9 +3,14 @@ import Component from './Component.js';
 class TodoItem extends Component {
     renderTemplate() {
         const todo = this.props.todo;
+        let checked = '';
+
+        if(todo.completed) {
+            checked = 'checked';
+        }
         return /*html*/`
         <label for="${todo.label}">
-            <input checked value="${todo.label}" type="checkbox"> ${todo.task}
+            <input ${checked} value="${todo.label}" type="checkbox"> ${todo.task}
         </label>
         `;
     }
